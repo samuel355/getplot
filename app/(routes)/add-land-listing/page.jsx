4d08c194c5bed363f3690acd583910f236765fec
Samuel Osei Adu <asamuel355@yahoo.com>
2024-11-04 11:51:38 +0000

    removed-x-dep

--- app/(routes)/add-land-listing/page.jsx
+++ app/(routes)/add-land-listing/page.jsx
@@ -15,7 +15,6 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 import { supabase } from "@/utils/supabase/client";
 import { useUser } from "@clerk/nextjs";
-x;
 import { Formik } from "formik";
 import { Loader } from "lucide-react";
 import { useRouter } from "next/navigation";

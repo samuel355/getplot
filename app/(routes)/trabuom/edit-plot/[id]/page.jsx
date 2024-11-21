bc1f9025f1680de12af4c5e7af70198a5d6f32e4
Samuel Osei Adu <asamuel355@yahoo.com>
2024-11-21 09:45:52 +0000

    update on legon-hills reserve plot

--- app/(routes)/trabuom/edit-plot/[id]/page.jsx
+++ app/(routes)/trabuom/edit-plot/[id]/page.jsx
@@ -323,7 +323,7 @@ const EditPlot = () => {
       toast.success("Plot  updated successfully");
       setLoader2(false);
       setTimeout(() => {
-        window.location.reload();
+        window.location.href='/trabuom';
       }, 1100);
     }
     if (error) {

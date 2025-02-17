6f91492584d6ccef25d54f16ac7c7864bdc2a904
Samuel Osei Adu <asamuel355@yahoo.com>
2025-02-17 10:28:13 +0000

    update yabi

--- app/(routes)/yabi/edit-plot/[id]/page.jsx
+++ app/(routes)/yabi/edit-plot/[id]/page.jsx
@@ -260,7 +260,7 @@ const EditPlot = () => {
       toast.success("Plot details updated successfully");
       setLoader2(false);
       setTimeout(() => {
-        window.location.reload();
+        router.replace('/yabi');
       }, 1500);
     }
     if (error) {

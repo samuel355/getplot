26dd5c3feba868dedf5bdfb2d13cd80adfe26361
Samuel Osei Adu <asamuel355@yahoo.com>
2025-04-02 15:41:29 +0000

    update on plot status

--- app/(routes)/trabuom/page.jsx
+++ app/(routes)/trabuom/page.jsx
@@ -535,6 +535,20 @@ const Map = () => {
         <h1 className="font-bold text-lg my-4 text-center capitalize">
           TRABOUM SITE
         </h1>
+        <div className="w-full z-10 flex md:hidden flex-row items-center mb-4">
+          <div className="flex w-full items-center space-x-3">
+            <div className="w-4 h-4 bg-green-800 rounded-sm"></div>
+            <span>Available</span>
+          </div>
+          <div className="flex w-full items-center space-x-3">
+            <div className="w-4 h-4 bg-black rounded-sm"></div>
+            <span>Reserved</span>
+          </div>
+          <div className="flex w-full items-center space-x-3">
+            <div className="w-4 h-4 bg-red-600 rounded-sm"></div>
+            <span>Sold</span>
+          </div>
+        </div>
         
         {/* Map container */}
         <div className="w-full flex flex-col items-center justify-center relative">
@@ -584,17 +598,17 @@ const Map = () => {
               }}
             >
               {/* Legend for plot status */}
-              <div className="absolute w-40 top-20 left-0 bg-white/90 shadow-md rounded-md z-10">
-                <div className="p-2 bg-gray-100 font-medium rounded-t-md">Legend</div>
-                <div className="flex gap-3 max-w-36 items-center pl-2 pt-3">
+              <div className="absolute w-40 top-20 left-0 bg-white/90 shadow-md rounded-md z-10 hidden md:flex md:flex-col justify-center items-center">
+                <div className="p-2 bg-gray-100 font-medium rounded-t-md w-full">Legend</div>
+                <div className="flex gap-3 w-full items-center pl-2 pt-3">
                   <div className="w-4 h-4 bg-green-800 rounded-sm"></div>
                   <span>Available</span>
                 </div>
-                <div className="flex gap-3 max-w-36 items-center pl-2 mt-2">
+                <div className="flex gap-3 w-full items-center pl-2 mt-2">
                   <div className="w-4 h-4 bg-black rounded-sm"></div>
                   <span>Reserved</span>
                 </div>
-                <div className="flex gap-3 max-w-36 items-center pl-2 mt-2 pb-3">
+                <div className="flex gap-3 w-full items-center pl-2 mt-2 pb-3">
                   <div className="w-4 h-4 bg-red-600 rounded-sm"></div>
                   <span>Sold</span>
                 </div>

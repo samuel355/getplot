52fb489344cf08db34ef49fae9311098b0a510da
Samuel Osei Adu <asamuel355@yahoo.com>
2025-04-02 18:14:28 +0000

    update land status

--- app/(routes)/trabuom/page.jsx
+++ app/(routes)/trabuom/page.jsx
@@ -599,7 +599,7 @@ const Map = () => {
             >
               {/* Legend for plot status */}
               <div className="absolute w-40 top-20 left-0 bg-white/90 shadow-md rounded-md z-10 hidden md:flex md:flex-col justify-center items-center">
-                <div className="p-2 bg-gray-100 font-medium rounded-t-md w-full">Legend</div>
+                <div className="p-2 bg-gray-100 font-medium rounded-t-md w-full">Land Status</div>
                 <div className="flex gap-3 w-full items-center pl-2 pt-3">
                   <div className="w-4 h-4 bg-green-800 rounded-sm"></div>
                   <span>Available</span>

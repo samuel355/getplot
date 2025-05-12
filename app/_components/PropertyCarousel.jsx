81f91c0b39e88181b5c1349d3e0a909d1e2d101c
Samuel Osei Adu <asamuel355@yahoo.com>
2025-05-12 12:13:11 +0000

    update property caroursel

--- app/_components/PropertyCarousel.jsx
+++ app/_components/PropertyCarousel.jsx
@@ -84,7 +84,9 @@ const PropertyCarousel = () => {
               />
               <div className="absolute top-2 right-2">
                 <span className="bg-black/70 text-white text-xs px-2 py-1 rounded-md capitalize">
-                  {property.type}
+                  {property.listing_type === 'rent' ? 'For Rent' : 
+                   property.listing_type === 'airbnb' ? 'Airbnb' : 
+                   property.type}
                 </span>
               </div>
             </div>

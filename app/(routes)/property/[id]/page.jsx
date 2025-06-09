3a87e382cca7d9ea38eb17229db80e304e37f0da
Samuel Osei Adu <asamuel355@yahoo.com>
2025-06-09 10:05:26 +0000

    listing property user detail

--- app/(routes)/property/[id]/page.jsx
+++ app/(routes)/property/[id]/page.jsx
@@ -741,16 +741,19 @@ export default function PropertyPage() {
                 </div>
                 <div>
                   <h4 className="font-medium">
-                    {selectedProperty?.user_email}
+                    {/* {selectedProperty?.user_email} */}
+                    CTO (Get One Plot)
+                    
                   </h4>
                   <p className="text-sm text-gray-600 font-medium">
-                    Admin (Get One Plot)
+                    landandhomesconsult@gmail.com
                   </p>
                 </div>
               </div>
               <div className="flex flex-col space-y-2">
+                {/* ${selectedProperty?.contact} */}
                 <a
-                  href={`tel:${selectedProperty?.contact}`}
+                  href={`tel:0548554216`}
                   className="flex items-center text-gray-700 hover:text-primary"
                 >
                   <svg
@@ -767,10 +770,12 @@ export default function PropertyPage() {
                       d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                     />
                   </svg>
-                  {`0${selectedProperty?.contact}`}
+                  {/* {`0${selectedProperty?.contact}`} */}
+                  0548554216
                 </a>
+                {/* selectedProperty?.user_email */}
                 <a
-                  href={`mailto:${selectedProperty?.user_email}`}
+                  href={`mailto:landandhomesconsult@gmail.com`}
                   className="flex items-center text-gray-700 hover:text-primary"
                 >
                   <svg
@@ -787,7 +792,8 @@ export default function PropertyPage() {
                       d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                     />
                   </svg>
-                  {selectedProperty?.user_email}
+                  {/* {selectedProperty?.user_email} */}
+                  landandhomesconsult@gmail.com
                 </a>
               </div>
             </div>
